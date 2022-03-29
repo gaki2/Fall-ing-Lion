446a8c7d79c259d25f795d9fe333f25677142e9e
ByeonggakYu <qudrkr0815@korea.ac.kr>
2022-03-29 21:13:08 +0900

    a

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import { normalDistribution } from "./normal.js";
 
 export default class App {
   constructor() {
-    console.log(3321111);
     this.canvas = document.createElement("canvas");
     this.ctx = this.canvas.getContext("2d");
     document.body.appendChild(this.canvas);
